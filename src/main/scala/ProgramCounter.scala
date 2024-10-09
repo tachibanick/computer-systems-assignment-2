e2e31d4b3801ad99c70b25762d65333e8aa72a3f
Nicholas Lentfer Tachibana Kristiansen <nicholas@kristiansenz.com>
2024-10-09 13:23:22 +0200

    init

---
+++ src/main/scala/ProgramCounter.scala
@@ -0,0 +1,15 @@
+import chisel3._
+import chisel3.util._
+
+class ProgramCounter extends Module {
+  val io = IO(new Bundle {
+    val stop = Input(Bool())
+    val jump = Input(Bool())
+    val run = Input(Bool())
+    val programCounterJump = Input(UInt(16.W))
+    val programCounter = Output(UInt(16.W))
+  })
+
+  //Implement this module here (respect the provided interface, since it used by the tester)
+
+}
