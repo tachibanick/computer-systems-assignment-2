bf3fd9534b29346eee603f56fa04ac961a213ccf
Peter Graugaard <git@graugaard.me>
2024-10-09 15:12:06 +0200

    WIP good luck

--- src/main/scala/ProgramCounter.scala
+++ src/main/scala/ProgramCounter.scala
@@ -10,6 +10,12 @@ class ProgramCounter extends Module {
     val programCounter = Output(UInt(16.W))
   })
 
-  //Implement this module here (respect the provided interface, since it used by the tester)
+  val reg = RegInit(0.U(16.W))
 
+  //Implement this module here (respect the provided interface, since it used by the tester)
+  when (io.run && !io.stop && !io.jump) {
+    io.programCounter := io.programCounter + 1.U
+  } .elsewhen (io.run && !io.stop && io.jump) {
+    io.programCounter := io.programCounterJump
+  }
 }
