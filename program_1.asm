c938130487d42b01cdcb366d0c4deb955c179b26
Peter Graugaard <git@graugaard.me>
2024-10-23 16:25:51 +0200

    Add the compiler

---
+++ program_1.asm
@@ -0,0 +1,56 @@
+LI R1 0 ; x
+LI R2 0 ; y
+LI R3 20 ; size
+MULT R4 R3 R3 ; area
+LI R5 19 ; max index
+JEQ 56 R2 R3                   ; y loop "56" is first line out of y loop
+JEQ 54 R1 R3                  ; x loop "54" is first line out of x loop
+LI R0 0 ;edge detection                    ; edge detecition 48 is line of "make dark"
+JEQ 48 R2 R0  ; y=0
+JEQ 48 R2 R5  ; y=19
+JEQ 48 R1 R0  ; x=0
+JEQ 48 R1 R5  ; x=19
+MULT R6 R2 R3 ; img_addr = y*size              ; interior pixel
+ADD R6 R1 R6 ; img_addr = x + img_addr
+LD R7 R6 ; pixel = load img_addr
+JEQ 48 R7 R0  ; if pixel = 0
+SUBI R0 R2 1 ; y-1                             ; neighborup
+MULT R6 R0 R3 ; img_addr = (y-1) * size
+ADD R6 R1 R6 ; img_addr = x + img_addr
+LD R7 R6 ; neigbhorup = load img_addr
+LI R0 0
+JEQ 48 R7 R0  ; make dark if neighbor is dark
+ADDI R0 R2 1 ; y+1                           ; neighbordown
+MULT R6 R0 R3 ; img_addr = (y+1) * size
+ADD R6 R1 R6 ; img_addr = x + img_addr
+LD R7 R6 ; neigbhordown = load img_addr
+LI R0 0
+JEQ 48 R7 R0  ; make dark if neighbor is dark
+SUBI R0 R1 1 ; x-1                           ; neighborleft
+MULT R6 R2 R3 ; img_addr = y * size
+ADD R6 R0 R6 ; img_addr = (x-1) + img_addr
+LD R7 R6 ; neigbhorleft = load img_addr
+LI R0 0
+JEQ 48 R7 R0  ; make dark if neighbor is dark
+ADDI R0 R1 1 ; x+1                            ; neighborright
+MULT R6 R2 R3 ; img_addr = y * size
+ADD R6 R0 R6 ; img_addr = (x+1) + img_addr
+LD R7 R6 ; neigbhorup = load img_addr
+LI R0 0
+JEQ 48 R7 R0 ; make dark if neighbor is dark
+MULT R6 R2 R3 ; img_addr = y*size                  ; make light
+ADD R6 R1 R6 ; img_addr = x + img_addr
+ADD R6 R4 R6 ; img_addr = area + img_addr
+LI R0 255 
+SD R0 R6 ; store 255 at address + area
+ADDI R1 R1 1 ;x++
+JUMP 7
+MULT R6 R2 R3 ; img_addr = y*size              ; make dark
+ADD R6 R1 R6 ; img_addr = x + img_addr
+LI R0 0
+SD R0 R6
+ADDI R1 R1 1   ;                  ; end of x loop
+JUMP 7		
+ADDI R2 R2 1 ;                  ; end of y loop
+JUMP 6
+END
---
+++ src/compiler/compiler.py
@@ -0,0 +1,105 @@
+# Read file from path in 1st arg
+
+import sys
+
+op_code_map = {
+    "ADD": "000000",
+    "SUB": "000001",
+    "MULT": "000010",
+    "ADDI": "001000",  # Add immediate
+    "SUBI": "001001",  # Subtract immediate
+    "LD": "010000",  # Load data
+    "SD": "010001",  # Store data
+    "LI": "011000",  # Load immediate
+    "JEQ": "100000",  # Jump if equal
+    "JUMP": "100001",  # Jump
+    "END": "110000",
+}
+
+
+def read_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        sys.exit(1)
+
+
+def reg(register: str):
+    """
+    Convert register to 4-bit binary. First character is always R
+    """
+    return format(int(register[1:]), "04b")
+
+
+def imm(imm: str):
+    """
+    Convert immediate value to 16-bit binary
+    """
+    return format(int(imm), "016b")
+
+
+def unused(bits: int):
+    return "0" * bits
+
+
+def parse_line(line: str):
+    match line.split():
+        case ["ADD", dst, src1, src2]:
+            return f"{op_code_map['ADD']}{reg(dst)} {reg(src1)} {reg(src2)}{unused(14)}"
+        case ["SUB", dst, src1, src2]:
+            return f"{op_code_map['SUB']}{reg(dst)} {reg(src1)} {reg(src2)}{unused(14)}"
+        case ["MULT", dst, src1, src2]:
+            return f"{op_code_map['MULT']}{reg(dst)}{reg(src1)} {reg(src2)}{unused(14)}"
+        case ["ADDI", dst, src1, im]:
+            return f"{op_code_map['ADDI']} {reg(dst)} {reg(src1)} {unused(2)} {imm(im)}"
+        case ["SUBI", dst, src1, im]:
+            return f"{op_code_map['SUBI']} {reg(dst)} {reg(src1)} {unused(2)} {imm(im)}"
+        case ["LD", dst, addr]:
+            return f"{op_code_map['LD']} {reg(dst)} {reg(addr)} {unused(18)}"
+        case ["SD", src, addr]:
+            return f"{op_code_map['SD']} {reg(src)} {reg(addr)} {unused(18)}"
+        case ["LI", dst, im]:
+            return f"{op_code_map['LI']} {reg(dst)} {unused(6)} {imm(im)}"
+        case ["JEQ", addr, src1, src2]:
+            return f"{op_code_map['JEQ']} {reg(src1)} {reg(src2)}{unused(2)}{imm(addr)}"
+        case ["JUMP", addr]:
+            return f"{op_code_map['JUMP']} {unused(10)} {imm(addr)}"
+        case ["END"]:
+            return f"{op_code_map['END']} {unused(26)}"
+
+    return line
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: compiler.py <file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    file_contents = read_file(file_path)
+
+    lines = file_contents.split("\n")
+
+    # strip and remove empty lines and everything after ; in each line
+    lines = [line.split(";")[0].strip() for line in lines if len(line)]
+
+    # parse lines with parse_line
+    lines = [parse_line(line).replace(" ", "") for line in lines]
+
+    # Print lines where length is not 32
+    for i, line in enumerate(lines):
+        if len(line) != 32:
+            print(f"Line {i+1} has length {len(line)}")
+
+    print("\n".join(lines))
+    print()
+
+    # Convert each binary string to 8 digit hex
+    hex_lines = [hex(int(line, 2))[2:].zfill(8) for line in lines]
+    print("\n".join(hex_lines))
+
+
+if __name__ == "__main__":
+    main()
